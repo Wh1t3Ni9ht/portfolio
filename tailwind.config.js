7fdf819450e3e3f0ccb98b673bc424b42dd7f4cc
Wh1t3 Ni9ht <wh1t3ni9ht@gmail.com>
2022-05-17 17:20:41 +0300

    extend custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './components/**/*.{html,js, jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navbar: '#242526',
+      },
+    },
   },
   plugins: [],
 };
