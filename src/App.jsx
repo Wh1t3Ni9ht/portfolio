4706e4b591f7b4eb1c80a630251092e7dccfb3c1
Wh1t3 Ni9ht <wh1t3ni9ht@gmail.com>
2022-05-21 17:31:20 +0300

    import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useRef } from 'react';
+import { Navbar, Overview, Projects, ScrollDotIndicator, Skills } from './components';
+
 function App() {
-  return <div>app</div>;
+  const appRef = useRef();
+
+  return (
+    <div
+      ref={appRef}
+      className="snap-y snap-mandatory scroll-smooth overflow-scroll h-[calc(100vh-88px)] lg:h-auto lg:snap-none lg:overflow-visible"
+    >
+      <ScrollDotIndicator ref={appRef} />
+      <Navbar />
+      <Overview />
+      <Skills />
+      <Projects />
+    </div>
+  );
 }
 
 export default App;
