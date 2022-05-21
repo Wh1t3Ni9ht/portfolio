1cddbbbbcf9033169a3f16369f7713c8d4e31139
Wh1t3 Ni9ht <wh1t3ni9ht@gmail.com>
2022-05-21 17:31:42 +0300

    export new componets

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
-export { default as Navbar } from './Navbar';
 export { default as Container } from './Container';
+export { default as Navbar } from './Navbar';
+export { default as Overview } from './Overview';
+export { default as Projects } from './Projects';
+export { default as ScrollDotIndicator } from './ScrollDotIndicator';
+export { default as Skills } from './Skills';
